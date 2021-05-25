0ff4ac9cef4c1383ce2763f893875329a65ac7f3
Doho-K <kimdohwa2@naver.com>
2021-05-25 23:28:19 +0900

    Local File Uploaded
    Local file from desktop uploaded

---
+++ Beginner platformer/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    protected private Animator anim;
+    protected private Rigidbody2D rb;
+    protected private AudioSource death;
+
+    protected virtual void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        death = GetComponent<AudioSource>();
+    }
+
+
+    public void JumpedOn()
+    {
+        rb.velocity = Vector2.zero;
+        death.Play();
+        anim.SetTrigger("Death");
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        GetComponent<Collider2D>().enabled = false;
+    }
+
+    private void Death()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
